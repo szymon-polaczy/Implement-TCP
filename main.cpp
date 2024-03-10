2cad6d8e2f0a5c5bba3c29ee1587d56db19a0935
Szymon Polaczy <thomas_frey@tutanota.com>
2024-03-10 11:37:30 +0100

    I can read the buffer but I have no idea what's happening around me and why

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
+#include <bitset>
 #include "tun_tap/tun_tap.hpp"
+
+using namespace std;
+
+//TODO: Implement this to such a level where we can connect with somethin real on the internet
+//TODO: Read RFC 793
+//TODO: Read RFC 1180 - A TCP/IP Tutorial
+//TODO: Read RFC 7414 - Lists all functionalities that are needed to implement
+//TODO: Read RFC 2398 - Some testing tools for TCP Implementers
+//TODO: Read RFC 2525 - Known TCP Implementation Problems
 
 int main() {
 	tun_tap::iface* test_interface = new tun_tap::iface("tun0", tun_tap::mode::TUN);
 	//int buf[2] = {0, 1504};
-	char buf[2048];	
-	std::cout << test_interface->read(&buf, 64);
+	char buf[1504];
+	while (true) {
+	cout << test_interface->read(&buf, 64) << endl;
+	/*for(int i = 0; i < 1504; i++) {
+		std::bitset<8> x(buf[i]);
+		cout << x << endl;
+	}*/
+	/*unsigned short p = (buf[0] << 8) | buf[1];
+	unsigned short p2 = (buf[2] << 8) | buf[3];
+	cout << p << endl;
+	cout << p2 << endl;*/
+	}
 	return 0;
 }
